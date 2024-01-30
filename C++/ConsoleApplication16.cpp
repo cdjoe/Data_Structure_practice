be49df71d4fbe7bc5dfc14cb4d1182a8df2e556d
JI-JIA <43168295+cdjoe@users.noreply.github.com>
2024-01-30 19:53:48 +0800

    Update ConsoleApplication16.cpp

--- C++/ConsoleApplication16.cpp
+++ C++/ConsoleApplication16.cpp
@@ -144,8 +144,6 @@ public:
     }
 };
 
-
-
 int main(void)
 {
     /*3*/
@@ -239,13 +237,10 @@ int main(void)
     CBox b2;
     b2.show();
 
-
     return 0;
 }
 
-
-
-int max(int a, int b, int c) {   //要記起來~
+int max(int a, int b, int c) {   //簫n簞O簞_穡��~
     int n = a;
     if (n<b)
         n = b;
@@ -284,14 +279,3 @@ int add(int num1, int num2) {
     a = num1 + num2;
     return a;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
